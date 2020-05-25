b12eb28ff572d5be54a884f6ef73c11c1fcd4f28
Pablo Alvarez Corredera <rosepac21@gmail.com>
2020-05-25 15:02:18 +0200

    Act. Javascript

--- assets/js/afiliados.js
+++ assets/js/afiliados.js
@@ -1,5 +1,1 @@
-<script type="text/javascript">
-    var ouo_token = 'eDdvDu7k';
-    var domains = ['udemy.com','click.linksynergy.com','edx.com','miriadax.net','devcode.la','crehana.com']; 
-</script>
-<script src="//cdn.ouo.io/js/full-page-script.js"></script>
+var ouo_token = 'eDdvDu7k'; var domains = ['udemy.com','click.linksynergy.com','edx.com','miriadax.net','devcode.la','crehana.com'];
